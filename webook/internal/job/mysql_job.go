61e80908ba8e28352223e4ac629b5cb7a139565e
zmsocc <zmsocc@163.com>
2025-10-10 21:06:34 +0800

    10.10

--- webook/internal/job/mysql_job.go
+++ webook/internal/job/mysql_job.go
@@ -2,11 +2,14 @@ package job
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/zmsocc/practice/webook/internal/domain"
 	"github.com/zmsocc/practice/webook/internal/service"
 	"github.com/zmsocc/practice/webook/pkg/logger"
 	"golang.org/x/sync/semaphore"
+	"net/http"
 	"time"
 )
 
@@ -18,6 +21,34 @@ type Executor interface {
 	// 具体实现来控制
 	// 真正去执行一个任务
 	Exec(ctx context.Context, j domain.Job) error
+}
+
+type HttpExecutor struct {
+}
+
+func (h *HttpExecutor) Name() string {
+	return "http"
+}
+
+func (h *HttpExecutor) Exec(ctx context.Context, j domain.Job) error {
+	type Config struct {
+		Endpoint string
+		Method   string
+	}
+	var cfg Config
+	err := json.Unmarshal([]byte(j.Cfg), &cfg)
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest(cfg.Method, cfg.Endpoint, nil)
+	if err != nil {
+		return err
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if resp.StatusCode != http.StatusOK {
+		return errors.New("执行失败")
+	}
+	return nil
 }
 
 type LocalFuncExecutor struct {
